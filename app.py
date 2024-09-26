535c6a54061335f6ef22c0edd3c64de8eab73aab
phuchoangdfir <phuc.hoang@dfir.vn>
2024-09-26 18:25:12 +0700

    update: update to test ci/cd 3

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World Matt! This is code change"
+    return "Hello World Matt! This is code change 2"
 
 
 if __name__=="__main__":
