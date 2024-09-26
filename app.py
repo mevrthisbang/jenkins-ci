e3b99ca867ca14329282e90ab938767896a5a32d
phuchoangdfir <phuc.hoang@dfir.vn>
2024-09-26 20:52:32 +0700

    update: test pipeline again to make sure

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World Matt! This is code change 3"
+    return "Hello World Matt! This is code change ehehe"
 
 
 if __name__=="__main__":
